3053602afdacac04721cc506bc39989fe04d78c1
Mohd Makki <mohd.makki.sh@gmail.com>
2018-10-03 20:10:42 +0530

    Initial commit

---
+++ baseApp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=254, required=True, label='First Name')
+    last_name = forms.CharField(max_length=254, required=True, label='Last Name')
+    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
+    dob = forms.DateField(widget=forms.SelectDateWidget(years=range(1980, 2018)))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'dob')
